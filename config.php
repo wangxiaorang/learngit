e150fc2685ff7c13b568132901344797470c357e
汪让和 <1217445199@qq.com>
2019-11-28 15:54:01 +0800

    测试提交config.php

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/4/4
+ * Time: 16:31
+ */
+
+return [
+
+    //允许访问初始化接口的IP白名单
+    "allow_ip" => [
+        "127.0.0.1" //本机
+        , "218.17.161.65" //公司
+        , "58.250.18.35" //公司
+
+    ]
+];
